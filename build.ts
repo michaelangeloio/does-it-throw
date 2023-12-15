d72dafe36634396ff1f68e2b1dc8651af24fe050
michaelangeloio <email@michaelangelo.io>
2023-12-15 11:33:26 -0500

    fix: builder bug

--- build.ts
+++ build.ts
@@ -2,123 +2,220 @@
 import { watch } from 'chokidar'
 import { build as esbuild } from 'esbuild'
 import copy from 'esbuild-plugin-copy'
+import { Errorlike, readableStreamToText } from 'bun'
 
-const log = (...args: any[]) => console.log('\x1b[36m%s\x1b[0m', '[watcher]', ...args)
-const logError = (...args: any[]) => console.log('\x1b[31m%s\x1b[0m', ...args)
+const log = (...args: any[]) => console.log('\x1b[36m%s\x1b[0m', '[builder]', ...args)
+const logError = (...args: any[]) => console.log('\x1b[31m%s\x1b[0m', '[builder]', ...args)
 
-const compile = async ({ path, ready, init }: { path: string; ready: boolean; init: boolean }) => {
-  if (!ready) {
-    return
-  }
-  const plugins: any = []
-  if (path.includes('.rs') || init) {
-    plugins.push(
-      copy({
-        resolveFrom: 'cwd',
-        assets: {
-          from: ['./server/src/rust/**/*'],
-          to: ['./server/out'],
-        },
-        watch: true,
-      }) as any,
-    )
-    // TODO - use bun to bundle the code when it supports CJS (or vscode supports ESM)
-    const command = 'wasm-pack build crates/does-it-throw-wasm --target nodejs --out-dir ../../server/src/rust'
-    try {
-      log('building wasm')
-      const result = Bun.spawnSync(command.split(' '), {
-        cwd: import.meta.dir,
-        onExit: (proc, exitCode, signalCode, error) => {
-          logError(proc, exitCode, signalCode, error)
-          return
-        },
-      })
+const isError = (exitCode: number) => {
+	return exitCode !== 0
+}
 
-      log(result.stderr.toString())
-      log(result.stdout.toString())
-    } catch (e) {
-      log(e)
-    }
-  }
+const compile = async ({
+	path,
+	ready,
+	init
+}: {
+	path: string
+	ready: boolean
+	init: boolean
+}) => {
+	if (!ready) {
+		return
+	}
+	const plugins: any = []
+	const errors = []
 
-  const compileClient = async () => {
-    log('compiling client ts')
-    await esbuild({
-			minify: true,
-			sourcemap: true,
-			bundle: true,
-      outdir: 'client/out',
-      entryPoints: ['client/src/extension.ts'],
-			external: ['vscode'],
-			platform: 'node',
-      format: 'cjs',
-      tsconfig: 'client/tsconfig.json',
-      plugins,
-    })
-    log('done compiling client')
-  }
+	/**
+	 * Compile WASM if the file is a rust file or if this is the first time
+	 */
 
-  const compileServer = async () => {
-    log('compiling server ts')
-    await esbuild({
-      outdir: 'server/out',
-			minify: true,
-			bundle: true,
-			sourcemap: true,
-			platform: 'node',
-			external: ['vscode'],
-      entryPoints: ['server/src/server.ts'],
-      format: 'cjs',
-      tsconfig: 'server/tsconfig.json',
-      plugins,
-    })
-    log('done compiling server')
-  }
+	const compileWasm = async () => {
+		// TODO - use bun to bundle the code when it supports CJS (or vscode supports ESM)
+		const command = 'wasm-pack build crates/does-it-throw-wasm --target nodejs --out-dir ../../server/src/rust'
+		log('building wasm')
+		const { stdout, exited } = Bun.spawn(command.split(' '), {
+			cwd: __dirname
+		})
+		const [stdOut, exitCode] = await Promise.all([await readableStreamToText(stdout), exited])
+		if (isError(exitCode)) {
+			return {
+				isError: true,
+				error: stdOut,
+				type: 'wasm'
+			}
+		}
+		log('done building wasm', stdOut)
+		return {
+			isError: false,
+			error: null,
+			type: 'wasm'
+		}
+	}
 
-  await Promise.all([compileClient(), compileServer()])
+	const typeCheck = async (project: 'server' | 'client') => {
+		const command = `tsc -p ${project}/tsconfig.json --noEmit`
+		log('type checking')
+		const { exited, stdout } = Bun.spawn(command.split(' '), {
+			cwd: __dirname
+		})
+		const [stdOut, exitCode] = await Promise.all([await readableStreamToText(stdout), exited])
+		if (isError(exitCode)) {
+			return {
+				isError: true,
+				error: stdOut,
+				type: `type-check-${project}`
+			}
+		}
+		log('done type checking', stdOut)
+		return {
+			isError: false,
+			error: null,
+			type: `type-check-${project}`
+		}
+	}
+
+	/**
+	 * Compile/Bundle the client
+	 */
+	if (path.includes('.rs') || init) {
+		plugins.push(
+			copy({
+				resolveFrom: 'cwd',
+				assets: {
+					from: ['./server/src/rust/**/*'],
+					to: ['./server/out']
+				},
+				watch: true
+			}) as any
+		)
+	}
+	const compileClient = async () => {
+		try {
+			log('compiling client ts')
+			await esbuild({
+				minify: true,
+				sourcemap: true,
+				bundle: true,
+				outdir: 'client/out',
+				entryPoints: ['client/src/extension.ts'],
+				external: ['vscode'],
+				platform: 'node',
+				format: 'cjs',
+				tsconfig: 'client/tsconfig.json',
+				plugins
+			})
+			log('done compiling client')
+			return {
+				isError: false,
+				error: null,
+				type: 'client-compile'
+			}
+		} catch (error) {
+			return {
+				isError: true,
+				error,
+				type: 'client-compile'
+			}
+		}
+	}
+
+	/**
+	 * Compile/Bundle the server
+	 */
+	const compileServer = async () => {
+		try {
+			log('compiling server ts')
+			await esbuild({
+				outdir: 'server/out',
+				minify: true,
+				bundle: true,
+				sourcemap: true,
+				platform: 'node',
+				external: ['vscode'],
+				entryPoints: ['server/src/server.ts'],
+				format: 'cjs',
+				tsconfig: 'server/tsconfig.json',
+				plugins
+			})
+			log('done compiling server')
+			return {
+				isError: false,
+				error: null,
+				type: 'server-compile'
+			}
+		} catch (error) {
+			return {
+				isError: true,
+				error,
+				type: 'server-compile'
+			}
+		}
+	}
+
+	/**
+	 * Build everything in parallel
+	 */
+
+	const result = await compileWasm()
+	if (result.isError) {
+		errors.push(result)
+		return errors
+	}
+
+	errors.push(...(await Promise.all([compileClient(), compileServer(), typeCheck('server'), typeCheck('client')])))
+	return errors
 }
 
 async function main() {
-  const isWatch = process.execArgv.find((arg) => arg.includes('watch'))
-  if (!isWatch) {
-    log('not in watch mode, compiling once')
-    await compile({
-      init: true,
-      ready: true,
-      path: '',
-    })
-    return
-  }
+	const isWatch = process.execArgv.find((arg) => arg.includes('watch'))
+	if (!isWatch) {
+		log('not in watch mode, compiling once')
+		const errors = await compile({
+			init: true,
+			ready: true,
+			path: ''
+		})
+		const hasErrors = errors?.filter((error) => error.isError)
+		if (hasErrors?.length) {
+			logError('errors compiling')
+			for (const error of hasErrors) {
+				logError(error)
+			}
+			throw new Error('exiting')
+		}
+		return
+	}
 
-  const watcher = watch('**/*.{ts,rs}', {
-    ignored: '(node_modules|target)/**/*',
-  })
-  let ready = false
+	const watcher = watch('**/*.{ts,rs}', {
+		ignored: '(node_modules|target)/**/*'
+	})
+	let ready = false
 
-  watcher.on('ready', async () => {
-    log('ready')
-    ready = true
-    await compile({
-      init: true,
-      ready,
-      path: '',
-    })
-  })
+	watcher.on('ready', async () => {
+		log('ready')
+		ready = true
+		await compile({
+			init: true,
+			ready,
+			path: ''
+		})
+	})
 
-  watcher.on('change', async (path) => {
-    await compile({
-      init: false,
-      path,
-      ready,
-    })
-  })
-  watcher.on('add', async (path) => {
-    await compile({
-      path,
-      ready,
-      init: false,
-    })
-  })
+	watcher.on('change', async (path) => {
+		await compile({
+			init: false,
+			path,
+			ready
+		})
+	})
+	watcher.on('add', async (path) => {
+		await compile({
+			path,
+			ready,
+			init: false
+		})
+	})
 }
 
 main()
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import {
-  DidChangeConfigurationNotification,
-  InitializeParams,
-  InitializeResult,
-  ProposedFeatures,
-  TextDocumentSyncKind,
-  TextDocuments,
-  createConnection,
+	DidChangeConfigurationNotification,
+	InitializeParams,
+	InitializeResult,
+	ProposedFeatures,
+	TextDocumentSyncKind,
+	TextDocuments,
+	createConnection
 } from 'vscode-languageserver/node'
 
 import { access, constants, readFile } from 'fs/promises'
@@ -16,7 +16,6 @@ import path = require('path')
 const connection = createConnection(ProposedFeatures.all)
 
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
-
 let hasConfigurationCapability = false
 let hasWorkspaceFolderCapability = false
 // use if needed later
@@ -25,73 +24,84 @@ let hasWorkspaceFolderCapability = false
 let rootUri: string | undefined | null
 
 connection.onInitialize((params: InitializeParams) => {
-  const capabilities = params.capabilities
+	const capabilities = params.capabilities
 
-  hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration)
-  hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders)
-  // use if needed later
-  // hasDiagnosticRelatedInformationCapability = !!(
-  // 	capabilities.textDocument &&
-  // 	capabilities.textDocument.publishDiagnostics &&
-  // 	capabilities.textDocument.publishDiagnostics.relatedInformation
-  // )
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	)
+	hasWorkspaceFolderCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	)
+	// use if needed later
+	// hasDiagnosticRelatedInformationCapability = !!(
+	// 	capabilities.textDocument &&
+	// 	capabilities.textDocument.publishDiagnostics &&
+	// 	capabilities.textDocument.publishDiagnostics.relatedInformation
+	// )
 
-  const result: InitializeResult = {
-    capabilities: {
-      textDocumentSync: TextDocumentSyncKind.Incremental,
-    },
-  }
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental
+		}
+	}
 	if (params?.workspaceFolders && params.workspaceFolders.length > 1) {
-    throw new Error('This extension only supports one workspace folder at this time')
-  }
-  if (hasWorkspaceFolderCapability) {
-    result.capabilities.workspace = {
-      workspaceFolders: {
-        supported: false,
-      },
-    }
-  }
-  if (!hasWorkspaceFolderCapability) {
-    rootUri = params.rootUri
-  } else {
-    rootUri = params?.workspaceFolders?.[0]?.uri
-  }
+		throw new Error(
+			'This extension only supports one workspace folder at this time'
+		)
+	}
+	if (hasWorkspaceFolderCapability) {
+		result.capabilities.workspace = {
+			workspaceFolders: {
+				supported: false
+			}
+		}
+	}
+	if (!hasWorkspaceFolderCapability) {
+		rootUri = params.rootUri
+	} else {
+		rootUri = params?.workspaceFolders?.[0]?.uri
+	}
 
-  return result
+	return result
 })
 
 connection.onInitialized(() => {
-  if (hasConfigurationCapability) {
-    // Register for all configuration changes.
-    connection.client.register(DidChangeConfigurationNotification.type, undefined)
-  }
-  if (hasWorkspaceFolderCapability) {
-    connection.workspace.onDidChangeWorkspaceFolders((_event) => {
-      connection.console.log(`Workspace folder change event received. ${JSON.stringify(_event)}`)
-    })
-  }
+	if (hasConfigurationCapability) {
+		// Register for all configuration changes.
+		connection.client.register(
+			DidChangeConfigurationNotification.type,
+			undefined
+		)
+	}
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+			connection.console.log(
+				`Workspace folder change event received. ${JSON.stringify(_event)}`
+			)
+		})
+	}
 })
 
 type DiagnosticSeverity = 'Error' | 'Warning' | 'Information' | 'Hint'
 
 // The server settings
 interface Settings {
-  maxNumberOfProblems: number
-  throwStatementSeverity: DiagnosticSeverity
-  functionThrowSeverity: DiagnosticSeverity
-  callToThrowSeverity: DiagnosticSeverity
-  callToImportedThrowSeverity: DiagnosticSeverity
+	maxNumberOfProblems: number
+	throwStatementSeverity: DiagnosticSeverity
+	functionThrowSeverity: DiagnosticSeverity
+	callToThrowSeverity: DiagnosticSeverity
+	callToImportedThrowSeverity: DiagnosticSeverity
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
 const defaultSettings: Settings = {
-  maxNumberOfProblems: 1000000,
-  throwStatementSeverity: 'Hint',
-  functionThrowSeverity: 'Hint',
-  callToThrowSeverity: 'Hint',
-  callToImportedThrowSeverity: 'Hint',
+	maxNumberOfProblems: 1000000,
+	throwStatementSeverity: 'Hint',
+	functionThrowSeverity: 'Hint',
+	callToThrowSeverity: 'Hint',
+	callToImportedThrowSeverity: 'Hint'
 }
 // 👆 very unlikely someone will have more than 1 million throw statements, lol
 // if you do, might want to rethink your code?
@@ -101,153 +111,186 @@ let globalSettings: Settings = defaultSettings
 const documentSettings: Map<string, Thenable<Settings>> = new Map()
 
 connection.onDidChangeConfiguration((change) => {
-  if (hasConfigurationCapability) {
-    // Reset all cached document settings
-    documentSettings.clear()
-  } else {
-    globalSettings = <Settings>(change.settings.doesItThrow || defaultSettings)
-  }
+	if (hasConfigurationCapability) {
+		// Reset all cached document settings
+		documentSettings.clear()
+	} else {
+		globalSettings = <Settings>(change.settings.doesItThrow || defaultSettings)
+	}
 
-  // Revalidate all open text documents
-  documents.all().forEach(validateTextDocument)
+	// Revalidate all open text documents
+	// biome-ignore lint/complexity/noForEach: <explanation>
+	documents.all().forEach(validateTextDocument)
 })
 
 function getDocumentSettings(resource: string): Thenable<Settings> {
-  if (!hasConfigurationCapability) {
-		connection.console.log(`does not have config capability, using global settings: ${JSON.stringify(globalSettings)}`)
-    return Promise.resolve(globalSettings)
-  }
-  let result = documentSettings.get(resource)
-  if (!result) {
+	if (!hasConfigurationCapability) {
+		connection.console.log(
+			`does not have config capability, using global settings: ${JSON.stringify(
+				globalSettings
+			)}`
+		)
+		return Promise.resolve(globalSettings)
+	}
+	let result = documentSettings.get(resource)
+	if (!result) {
 		connection.console.log(`getting settings for ${resource}`)
-    result = connection.workspace.getConfiguration({
-      scopeUri: resource,
-      section: 'doesItThrow',
-    })
-    documentSettings.set(resource, result)
-  }
-  return result
+		result = connection.workspace.getConfiguration({
+			scopeUri: resource,
+			section: 'doesItThrow'
+		})
+		documentSettings.set(resource, result)
+	}
+	return result
 }
 
 // Only keep settings for open documents
 documents.onDidClose((e) => {
-  documentSettings.delete(e.document.uri)
+	documentSettings.delete(e.document.uri)
 })
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(async (change) => {
-  validateTextDocument(change.document)
+	validateTextDocument(change.document)
 })
 
 documents.onDidSave((change) => {
-  validateTextDocument(change.document)
+	validateTextDocument(change.document)
 })
 
 const _checkAccessOnFile = async (file: string) => {
-  try {
-    await access(file, constants.R_OK)
-    return Promise.resolve(file)
-  } catch (e) {
-    return Promise.reject(e)
-  }
+	try {
+		await access(file, constants.R_OK)
+		return Promise.resolve(file)
+	} catch (e) {
+		return Promise.reject(e)
+	}
 }
 
-const findFirstFileThatExists = async (uri: string, relative_import: string) => {
-  const isTs = uri.endsWith('.ts') || uri.endsWith('.tsx')
-  const baseUri = `${path.resolve(path.dirname(uri.replace('file://', '')), relative_import)}`
-  let files = Array(4)
-  if (isTs) {
-    files = [`${baseUri}.ts`, `${baseUri}.tsx`, `${baseUri}.js`, `${baseUri}.jsx`]
-  } else {
-    files = [`${baseUri}.js`, `${baseUri}.jsx`, `${baseUri}.ts`, `${baseUri}.tsx`]
-  }
-  return Promise.any(files.map(_checkAccessOnFile))
+const findFirstFileThatExists = async (
+	uri: string,
+	relative_import: string
+) => {
+	const isTs = uri.endsWith('.ts') || uri.endsWith('.tsx')
+	const baseUri = `${path.resolve(
+		path.dirname(uri.replace('file://', '')),
+		relative_import
+	)}`
+	let files = Array(4)
+	if (isTs) {
+		files = [
+			`${baseUri}.ts`,
+			`${baseUri}.tsx`,
+			`${baseUri}.js`,
+			`${baseUri}.jsx`
+		]
+	} else {
+		files = [
+			`${baseUri}.js`,
+			`${baseUri}.jsx`,
+			`${baseUri}.ts`,
+			`${baseUri}.tsx`
+		]
+	}
+	return Promise.any(files.map(_checkAccessOnFile))
 }
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-  let settings = await getDocumentSettings(textDocument.uri)
+	let settings = await getDocumentSettings(textDocument.uri)
 	if (!settings) {
 		// this should never happen, but just in case
-		connection.console.log(`No settings found for ${textDocument.uri}, using defaults`)
+		connection.console.log(
+			`No settings found for ${textDocument.uri}, using defaults`
+		)
 		settings = defaultSettings
 	}
-  try {
-    const opts = {
-      uri: textDocument.uri,
-      file_content: textDocument.getText(),
-      ids_to_check: [],
-      typescript_settings: {
-        decorators: true,
-      },
-      function_throw_severity: settings.functionThrowSeverity,
-      throw_statement_severity: settings.throwStatementSeverity,
-      call_to_imported_throw_severity: settings.callToImportedThrowSeverity,
-      call_to_throw_severity: settings.callToThrowSeverity,
-    } satisfies InputData
-    const analysis = parse_js(opts) as ParseResult
+	try {
+		const opts = {
+			uri: textDocument.uri,
+			file_content: textDocument.getText(),
+			ids_to_check: [],
+			typescript_settings: {
+				decorators: true
+			},
+			function_throw_severity: settings.functionThrowSeverity,
+			throw_statement_severity: settings.throwStatementSeverity,
+			call_to_imported_throw_severity: settings.callToImportedThrowSeverity,
+			call_to_throw_severity: settings.callToThrowSeverity
+		} satisfies InputData
+		const analysis = parse_js(opts) as ParseResult
 
-    if (analysis.relative_imports.length > 0) {
-      const resolvedImports = analysis.relative_imports.map((relative_import) => {
-        return findFirstFileThatExists(textDocument.uri, relative_import)
-      })
-      const files = await Promise.all(
-        resolvedImports.map(async (file) => {
-          try {
-            return readFile(await file, 'utf-8')
-          } catch (e) {
-            connection.console.log(`Error reading file ${e}`)
-            return undefined
-          }
-        }),
-      )
-      const analysisArr = files.map((file) => {
-        if (!file) {
-          return undefined
-        }
-        const opts = {
-          uri: textDocument.uri,
-          file_content: file,
-          ids_to_check: [],
-          typescript_settings: {
-            decorators: true,
-          },
-        } satisfies InputData
-        return parse_js(opts) as ParseResult
-      })
-      // TODO - this is a bit of a mess, but it works for now.
-      // The original analysis is the one that has the throw statements Map()
-      // We get the get the throw_ids from the imported analysis and then
-      // check the original analysis for existing throw_ids
-      // This allows to to get the diagnostics from the imported analysis (one level deep for now)
-      analysisArr.forEach((import_analysis) => {
-        if (!import_analysis) {
-          return
-        }
-        if (import_analysis.throw_ids.length) {
-          import_analysis.throw_ids.forEach((throw_id) => {
-            const newDiagnostics = analysis.imported_identifiers_diagnostics.get(throw_id)
-            if (newDiagnostics && newDiagnostics?.diagnostics?.length) {
-              analysis.diagnostics.push(...newDiagnostics.diagnostics)
-            }
-          })
-        }
-      })
-    }
+		if (analysis.relative_imports.length > 0) {
+			const resolvedImports = analysis.relative_imports.map(
+				(relative_import) => {
+					return findFirstFileThatExists(textDocument.uri, relative_import)
+				}
+			)
+			const files = await Promise.all(
+				resolvedImports.map(async (file) => {
+					try {
+						return readFile(await file, 'utf-8')
+					} catch (e) {
+						connection.console.log(`Error reading file ${e}`)
+						return undefined
+					}
+				})
+			)
+			const analysisArr = files.map((file) => {
+				if (!file) {
+					return undefined
+				}
+				const opts = {
+					uri: textDocument.uri,
+					file_content: file,
+					ids_to_check: [],
+					typescript_settings: {
+						decorators: true
+					}
+				} satisfies InputData
+				return parse_js(opts) as ParseResult
+			})
+			// TODO - this is a bit of a mess, but it works for now.
+			// The original analysis is the one that has the throw statements Map()
+			// We get the get the throw_ids from the imported analysis and then
+			// check the original analysis for existing throw_ids
+			// This allows to to get the diagnostics from the imported analysis (one level deep for now)
+			for (const import_analysis of analysisArr) {
+				if (!import_analysis) {
+					return
+				}
+				if (import_analysis.throw_ids.length) {
+					for (const throw_id of import_analysis.throw_ids) {
+						const newDiagnostics =
+							analysis.imported_identifiers_diagnostics.get(throw_id)
+						if (newDiagnostics?.diagnostics?.length) {
+							analysis.diagnostics.push(...newDiagnostics.diagnostics)
+						}
+					}
+				}
+			}
+		}
 
-    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics: analysis.diagnostics })
-  } catch (e) {
-    console.log(e)
+		connection.sendDiagnostics({
+			uri: textDocument.uri,
+			diagnostics: analysis.diagnostics
+		})
+	} catch (e) {
+		console.log(e)
 		connection.console.log(`Error parsing file ${textDocument.uri}`)
 		connection.console.log(`settings are: ${JSON.stringify(settings)}`)
-    connection.console.log(`Error: ${e instanceof Error ? e.message : JSON.stringify(e)} error`)
-    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics: [] })
-  }
+		connection.console.log(
+			`Error: ${e instanceof Error ? e.message : JSON.stringify(e)} error`
+		)
+		connection.sendDiagnostics({ uri: textDocument.uri, diagnostics: [] })
+	}
 }
 
 connection.onDidChangeWatchedFiles((_change) => {
-  // Monitored files have change in VSCode
-  connection.console.log(`We received an file change event ${_change}, not implemented yet`)
+	// Monitored files have change in VSCode
+	connection.console.log(
+		`We received an file change event ${_change}, not implemented yet`
+	)
 })
 
 // Make the text document manager listen on the connection
